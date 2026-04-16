6da8d591af47f71c22eeec4c81e87d6794211ee6
Thehalospartansa <thehalospartainsa@gmail.com>
2026-04-15 18:49:23 -0700

    Update fan_controller.py

--- system/hardware/fan_controller.py
+++ system/hardware/fan_controller.py
@@ -10,7 +10,7 @@ class FanController:
     self.controller = PIDController(k_p=0, k_i=4e-3, rate=rate)
 
   def update(self, cur_temp: float, ignition: bool) -> int:
-    self.controller.pos_limit = 100 if ignition else 25
+    self.controller.pos_limit = 100 if ignition else 100
     self.controller.neg_limit = 25 if ignition else 0
 
     if ignition != self.last_ignition:
